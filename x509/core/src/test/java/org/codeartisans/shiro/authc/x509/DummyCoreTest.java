ec32fa36b94694877af0748ba9188061a5206b7a
Paul Merlin <paul@nosphere.org>
2020-06-13 01:03:16 +0200

    Add dummy tests

---
+++ x509/core/src/test/java/org/codeartisans/shiro/authc/x509/DummyCoreTest.java
@@ -0,0 +1,15 @@
+package org.codeartisans.shiro.authc.x509;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyCoreTest
+{
+
+    @Test
+    public void test()
+    {
+        assertTrue( true );
+    }
+}
---
+++ x509/web/src/test/java/org/codeartisans/shiro/web/filter/authc/DummyWebTest.java
@@ -0,0 +1,15 @@
+package org.codeartisans.shiro.web.filter.authc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyWebTest
+{
+
+    @Test
+    public void test()
+    {
+        assertTrue( true );
+    }
+}
